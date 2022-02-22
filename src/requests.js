8339f5af1cfbbe1b8f476fd2edd80af75451cc7e
Li Crockett <ccrockett55@gmail.com>
2022-02-21 18:09:47 -0600

    Initial Commit

---
+++ src/requests.js
@@ -0,0 +1,12 @@
+const API_KEY = '9aead5952e0f1e9fcfe2b22412f6b0ed';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
